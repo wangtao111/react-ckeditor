c577848e122927b5d66c1df69db9296d46342374
zuilafeng <lfzhu@abcft.com>
2019-03-05 17:22:54 +0800

    git merge 合并

--- src/ckeditor/plugins/insertchart/plugin.js
+++ src/ckeditor/plugins/insertchart/plugin.js
@@ -1,12 +1,25 @@
 CKEDITOR.plugins.add('insertchart', {
     requires: 'widget',
-    init: function(editor) {
+    init(editor) {
         editor.widgets.add('insertchart', {
-            template: '<div id="chartContainer" class="container"></div>',
+            template: '<div id="chartWrapper" class="container"><div id="chartContainer"></div></div>',
             requireContent: 'div(container)',
             upcast: function(element) {
                 return element.name === 'div' && element.hasClass('container')
+            },
+            data() {
+                // 当数据变化时，再画chart，或擦了重画
+                const { chartOption } = this.data;
+                if(chartOption) {
+                    setTimeout(() => {
+                        Highcharts.chart(this.editor.document.getById('chartContainer').$, chartOption);
+                    }, 0)
+                }
             }
         });
+    },
+    afterInit: function() {
+        // 加载highchart.js，并在回调中处理画图表
+        CKEDITOR.scriptLoader.load('https://code.highcharts.com/highcharts.js');
     }
 });
--- src/components/Editor2/index.jsx
+++ src/components/Editor2/index.jsx
@@ -363,7 +363,7 @@ export default class Editor extends React.Component {
     render() {
         const {data, title} = this.state;
         const config = {
-            extraPlugins: 'autocomplete,textmatch,noteTemplates,insertchart,inserttable,easyimage,tableresizerowandcolumn,save-to-pdf',
+            extraPlugins: 'autocomplete,textmatch,notetemplates,insertchart,inserttable,easyimage,tableresizerowandcolumn,save-to-pdf',
             allowedContent: true,
             pdfHandler: 'http://www.baidu.com', // 下载pdf的地址
             height: 800,
@@ -396,8 +396,9 @@ export default class Editor extends React.Component {
             //             `
         };
 
-        CKEditor.editorUrl = 'http://localhost:5500/build/static/ckeditor/ckeditor.js';
-        return <div>
+        CKEditor.editorUrl = '/static/ckeditor/ckeditor.js';
+        // CKEditor.editorUrl = 'http://localhost:5500/build/static/ckeditor/ckeditor.js';
+        return <div>{/*  */}
             <input style={{fontSize: '22px', lineHeight: '60px', color: '#666', width: '100%', border: 'none', outline: 'none', display: title ? 'block' : 'none'}} type='textarea' value={title} onChange={this.titleChange}/>
             <CKEditor
                 ref={this.editorRef}
