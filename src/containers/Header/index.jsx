050145db0152ce795f42f38bc9c70220208ff628
zuilafeng <lfzhu@abcft.com>
2019-03-07 20:29:16 +0800

    调整了样式

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Radio } from 'antd';
+import { Radio, Input } from 'antd';
 import styled from 'styled-components';
 
 const AppHeader = styled.header`
@@ -16,31 +16,127 @@ const AppHeader = styled.header`
         font-weight: normal;
     }
 
+    h1,
+    .operation-mode {
+        vertical-align: middle;
+    }
+
     .operation-mode {
         display: inline-block;
         margin-left: 45px;
 
         .ant-radio-button-wrapper {
-            border-radius: 2px;
+            width: 60px;
             height: 24px;
             line-height: 24px;
-            width: 60px;
+            font-size: 12px;
+            padding: 0 5px;
+            color: rgba(255, 255, 255, 1);
+            border-color: transparent;
+            background-color: #5391ED;
+            opacity: 0.8;
+
+            &:first-child {
+                border-top-left-radius: 2px;
+                border-bottom-left-radius: 2px;
+            }
+
+            &:last-child {
+                border-top-right-radius: 2px;
+                border-bottom-right-radius: 2px;
+            }
+
+            &:not(:first-child)::before {
+                display: none;
+            }
+
+            &.selected {
+                border: 1px solid #2059AE;
+                background-color: #2B60AF;
+                opacity: 1;
+            }
+        }
+
+        .mode-icon {
+            display: inline-block;
+            width: 16px;
+            height: 12px;
+            margin-right: 4px; 
+            vertical-align: -1px;
+        }
+
+        .icon-normal {
+            background: url('${require('../../theme/images/icon_normal_mode.png')}') no-repeat scroll center center / contain;
+        }
+
+        .icon-edit {
+            background: url('${require('../../theme/images/icon_edit_mode.png')}') no-repeat scroll center center / contain;
+        }
+    }
+
+    .search-box {
+        float: right;
+        margin-right: 130px;
+        width: 220px;
+
+        &:after {
+            display: block;
+            content: '';
+            clear: both;
+        }
+
+        .search-ipt {
+            .ant-input {
+                border-radius: 22px;
+                font-size: 12px;
+                height: auto;
+            }
+        }
+        .icon-search {
+            display: inline-block;
+            width: 15px;
+            height: 15px;
+            background: url('${require('../../theme/images/icon_search.png')}') no-repeat scroll 0 0 / contain;
         }
     }
 `
 export default class Header extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeMode: 'edit'
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            activeMode: e.target.value
+        })
+    }
 
     render() {
+        const { activeMode } = this.state;
+
         return <AppHeader>
             <h1>金融云笔记</h1>
             <div className="operation-mode">
-                <Radio.Group defaultValue="normal" buttonStyle="solid">
-                    <Radio.Button value="normal">普通</Radio.Button>
-                    <Radio.Button value="edit">编辑</Radio.Button>
+                <Radio.Group defaultValue={ activeMode } buttonStyle="solid" onChange={ this.handleChange }>
+                    <Radio.Button 
+                        value="normal" 
+                        className={  activeMode === 'normal' ? 'selected' : undefined}>
+                        <i className="icon-normal mode-icon"></i>普通
+                    </Radio.Button>
+                    <Radio.Button 
+                        value="edit" 
+                        className={  activeMode === 'edit' ? 'selected' : undefined}>
+                        <i className="icon-edit mode-icon"></i>编辑
+                    </Radio.Button>
                 </Radio.Group>
+            </div>
+
+            <div className="search-box">
+                <Input prefix={<i className="icon-search"></i>} placeholder="搜索..." className="search-ipt"/>
             </div>
         </AppHeader>
     }
